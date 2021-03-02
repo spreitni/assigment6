3100d479ef09957176d744fd849394f1663760e6
Nicholas Spreitzer <nicholas.spreitzer@cuw.edu>
2021-03-01 19:35:16 -0600

    first commet

---
+++ LinkedList.cpp
@@ -0,0 +1,123 @@
+#include "LinkedList.hpp"
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+LinkedList::LinkedList()
+{
+    this->head = NULL;
+    this->tail = NULL;
+    this->count = 0;
+}
+
+void LinkedList::addFront(int value)
+{
+    
+    
+    Node* n = new Node(value);
+    Node* currNode = this->head;
+    
+    
+        //we have a list with at least one element in it, and we know that tail
+        //points to the end of that list.
+        //this->head->setNextNode(currNode->getNextNode());
+        this->head = n;
+        n->setNextNode(currNode);
+    
+}
+int LinkedList::removeFront()
+{
+    if(this->count > 0)
+    {
+        Node* n = this->head;
+        int value = n->getPayload();
+        Node* currNode = this->head;
+        if(this->count == 1)
+        {
+            this->head = NULL;
+            this->tail = NULL;
+            
+        }
+        else
+        {
+            Node* currNode = this->head;
+            while(currNode->getNextNode() != this->head)
+            {
+                currNode = currNode->getNextNode();
+            }
+            this->head = currNode;
+            this->head->setNextNode(NULL);
+        }
+        
+        delete(n); //if we didn't do this, we technically have a memory leak
+        return value;
+    }
+    
+}
+
+void LinkedList::addEnd(int value)
+{
+    Node* n = new Node(value);
+    if(head == NULL)
+    {
+        //we have the empty list
+        this->head = n;
+        this->tail = n;
+    }
+    else
+    {
+        //we have a list with at least one element in it, and we know that tail
+        //points to the end of that list.
+        this->tail->setNextNode(n);
+        this->tail = n;
+    }
+    this->count++;
+}
+
+int LinkedList::removeEnd()
+{
+    if(this->count > 0)
+    {
+        Node* n = this->tail;
+        int value = n->getPayload();
+        if(this->count == 1)
+        {
+            this->head = NULL;
+            this->tail = NULL;
+            
+        }
+        else
+        {
+            Node* currNode = this->head;
+            while(currNode->getNextNode() != this->tail)
+            {
+                currNode = currNode->getNextNode();
+            }
+            this->tail = currNode;
+            this->tail->setNextNode(NULL);
+        }
+        this->count--;
+        delete(n); //if we didn't do this, we technically have a memory leak
+        return value;
+    }
+    
+}
+void LinkedList::display()
+{
+    if(this->count == 0)
+    {
+        cout << "the empty list" << endl;
+    }
+    else
+    {
+        Node* currNode = this->head;
+        while(currNode->getNextNode() != NULL)
+        {
+            cout << currNode->getPayload() << " -> ";
+            currNode = currNode->getNextNode();
+        }
+        cout << currNode->getPayload() << endl;
+    }
+    
+}
--- driver.cpp
+++ driver.cpp
@@ -1,53 +1,121 @@
 #include "Card.hpp"
 #include "URL.hpp"
 #include <iostream>
-#include <string.h>
+#include <string>
+#include "myFunctions.hpp"
+#include "LinkedList.hpp"
 
 using namespace std;
 
-string JSONSTRING(string jsonString)
-{int count = 0;
-    for (int i=0;i<jsonString.length();i++)
-    {
-        
-        if(jsonString[i] ==  '[' )
-         {          
-             if(count==0)
-             {
-                jsonString[i] =' ';
-                count++;
-                cout<<jsonString.at(i);
-             }
-
-         }
-        if(jsonString[i]==']')
-        {
-            
-            if (count==0)
-            {
-                jsonString[i] =' ';
-            }
-            else
-            {
-                count--;
-                
-            }
-        }
-    }
-    
-    return jsonString;
-    
-}
 int main(int argc, char** argv)
 {
     //URL* u1 = new URL("https://api.hearthstonejson.com/v1/25770/enUS/cards.json");
     //cout << u1->getContents() << endl;
-    //cout << u1->getContents().length() << endl;
-    string jsonString = "[{\"fname\":\"val1\", \"lname\": \"val2\", \"sums\":[1,2,3]},{\"fname\":\"val3\", \"lname\":\"val4\"}]";
+    //string jsonString = "[{\"fname\":\"val1\", \"lname\": \"val2\", \"sums\":[1,2,3]},{\"fname\":\"val3\", \"lname\":\"val4\"}]";
     //cout << jsonString << endl;
-
-    cout<<JSONSTRING(jsonString); 
+    //processJSONObject(jsonString, 0);
+    LinkedList* ll = new LinkedList();
+    ll->addEnd(4);
+    ll->addEnd(6);
+    ll->addEnd(8);
+    ll->addEnd(2);
+    ll->addFront(2);
+    ll->display();
     
+    int value1 = ll->removeEnd();
+    int value2 = ll->removeEnd();
+    int value3 = ll->removeFront();
+    //ll->display();
+    cout << value1 << endl;
+    cout << value2 << endl;
+    cout << value3 << endl;
+    
+
     return 0;
 }
+/*
+string processJSONObject(string json, int start)
+{
+    if(json[start] == '[')
+    {
+        return processJSONArray(json, start);
+    }
+    else if(json[start] == '{')
+    {
+        //must be an json object
+        string temp = "";
+        int countCurlyBraces = 1;
+        for(int i = start+1; i < json.length(); i++)
+        {
+            if(json[i] == '{')
+            {
+                temp += '{';
+                string answer = processJSONObject(json, i);
+                temp += answer;
+                i += answer.length()+1;
+                countCurlyBraces++;
+            }
+            else if(json[i] == '}')
+            {
+                countCurlyBraces--;
+                if(countCurlyBraces == 0)
+                {
+                    cout << temp << endl;
+                    return temp;
+                }
+            }
+            else if(json[i] == '[')
+            {
+                temp += '[';
+                string answer = processJSONArray(json, i);
+                temp += answer;
+                i += answer.length()+1;
+            }
 
+            if(i < json.length())
+            {
+                temp = temp + json[i];
+            }  
+        }
+    }
+}
+    
+string processJSONArray(string json, int start)
+{
+    string temp = "";
+    int countSquareBrackets = 1;
+    //process contents of the array
+    for(int i = start+1; i < json.length(); i++)
+    {
+        if(json[i] == '[')
+        {
+            string answer = processJSONArray(json, i);
+            temp += answer;
+            i += answer.length()+1;
+            countSquareBrackets++;
+        }
+        else if(json[i] == ']')
+        {
+            countSquareBrackets--;
+            if(countSquareBrackets == 0)
+            {
+                cout << temp << endl;
+                return temp;
+                //return temp;
+            }
+        }
+        else if(json[i] == '{')
+        {
+            temp += '{';
+            string answer = processJSONObject(json, i);
+            temp += answer;
+            i += answer.length()+1;
+        }
+        
+        if(i < json.length())
+        {
+            temp = temp + json[i];
+        }  
+        
+    }
+} */
